48d13d6d67a56caa74e9848db934e8143d18c731
cfhj8520 <cfhj8520@naver.com>
2022-02-21 03:45:33 +0900

    Initial commit

---
+++ src/main/java/com/example/whateverback/WhateverBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.whateverback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhateverBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhateverBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/whateverback/WhateverBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whateverback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhateverBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
