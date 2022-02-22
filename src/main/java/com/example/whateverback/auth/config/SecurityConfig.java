b196ee9f6834e82655d63f082de1ccfe3ce4ab79
cfhj8520 <cfhj8520@naver.com>
2022-02-22 18:07:42 +0900

    conf : needless search auth

--- src/main/java/com/example/whateverback/auth/config/SecurityConfig.java
+++ src/main/java/com/example/whateverback/auth/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/auth/**", "/category/**").permitAll()
                 .antMatchers("/swagger-ui/**", "/swagger-resources/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
